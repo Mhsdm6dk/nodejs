dd7873f4148a09bb9ec2ddc3fc75b12b13cbdb5d
Nguyen Hanh <nguyenhanh3@telio.vn>
2022-07-16 13:16:21 +0700

    1h16

--- my-app/src/component/myRecord/myRecord.js
+++ my-app/src/component/myRecord/myRecord.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import styles from './styles.module.css';
+import my1 from '../../image/my1.png';
+import my2 from '../../image/my2.png';
+import my3 from '../../image/my3.png';
+import graph_background from '../../image/graph_background.png';
+import graph1 from '../../image/graph1.png';
+import graph2 from '../../image/graph2.png';
+import graph_number from '../../image/graph_number.png';
 function MyRecord() {
     return (
         <div className={styles.MyRecordBox}>
@@ -8,31 +15,391 @@ function MyRecord() {
                     <div className={styles.recordBoxB}>
                         <div className={styles.recordBox}>
                             <div className={styles.record}>
-                                <div >
-                                    <div className=''>
+                                <div className={styles.recordButtonBox}>
+                                    <div className={styles.recordButtonContent}>
                                         BODY RECORD
                                     </div>
-                                    <div className=''>
+                                    <div className={styles.recordButton}>
                                         自分のカラダの記録
                                     </div>
                                 </div>
+                                <img src={my1} className={styles.recordImage} />
                             </div>
                         </div>
                     </div>
                     <div className={styles.recordBoxB}>
                         <div className={styles.recordBox}>
                             <div className={styles.record}>
-
+                                <div className={styles.recordButtonBox}>
+                                    <div className={styles.recordButtonContent}>
+                                        MY EXERCISE
+                                    </div>
+                                    <div className={styles.recordButton}>
+                                        自分の運動の記録
+                                    </div>
+                                </div>
+                                <img src={my2} className={styles.recordImage} />
                             </div>
                         </div>
                     </div>
                     <div className={styles.recordBoxB}>
                         <div className={styles.recordBox}>
                             <div className={styles.record}>
+                                <div className={styles.recordButtonBox}>
+                                    <div className={styles.recordButtonContent}>
+                                        MY DIARY
+                                    </div>
+                                    <div className={styles.recordButton}>
+                                        自分の日記
+                                    </div>
+                                </div>
+                                <img src={my3} className={styles.recordImage} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.graphBoxWrapper}>
+                <div className={styles.graphBox}>
+                    <div className={styles.grapHeader}>
+                        <div className={styles.grapHeaderName}>
+                            BODY RECORD
+                        </div>
+                        <div className={styles.grapHeaderDate}>
+                            2021.05.21
+                        </div>
+                    </div>
+                    <div className={styles.graphBodyWrapper}>
+                        <div className={styles.graphBody}>
+                            <img className={styles.graph_background} src={graph_background} />
+                            <img className={styles.graph_image} src={graph2} />
+                            <img className={styles.graph_image} src={graph1} />
+                            <img className={styles.graph_number} src={graph_number} />
+                        </div>
+
+                    </div>
+                    <div className={styles.graphBodyFooter}>
+                        <div className={styles.listGraphButton}>
+                            <div className={styles.graphButtonBox}>
+                                <div className={styles.graphButton}>
+                                    日
+                                </div>
+                            </div>
+                            <div className={styles.graphButtonBox}>
+                                <div className={styles.graphButton}>
+                                    日
+                                </div>
+                            </div>
+                            <div className={styles.graphButtonBox}>
+                                <div className={styles.graphButton}>
+                                    日
+                                </div>
+                            </div>
+                            <div className={styles.graphButtonBox + " " + styles.choose}>
+                                <div className={styles.graphButton}>
+                                    日
+                                </div>
+                            </div>
+                            <div style={{ flex: 1 }}>
 
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className={styles.graphBoxWrapper}>
+                <div className={styles.graphBox}>
+                    <div className={styles.grapHeader}>
+                        <div className={styles.grapHeaderName}>
+                            MY EXERCISE
+                        </div>
+                        <div className={styles.grapHeaderDate}>
+                            2021.05.21
+                        </div>
+                    </div>
+                    <div className={styles.exerciseBody}>
+                        <div className={styles.listContentBox}>
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className={styles.contentBoxWrapper}>
+                                <div className={styles.contentBox}>
+                                    <div className={styles.contentLeft}>
+                                        <li className={styles.contentLeftName}>
+
+                                            家事全般（立位・軽い）
+
+                                            <div className={styles.contentLeft_number}>
+                                                26kcal
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div className={styles.contentRight}>
+                                        10 min
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
